668ac567df67275ce7365b221b88d5efa6a378a7
junkisai <jonki.beam@gmail.com>
2019-11-20 19:16:55 +0900

    feat: carousel-slider-item作成

--- src/components/carousel-slider-item.js
+++ src/components/carousel-slider-item.js
@@ -12,8 +12,17 @@ class CarouselSliderItem extends LitElement {
   render() {
     return html`
       <style>
+        :host {
+          border: solid 1px #0fdf75;
+          flex-shrink: 0;
+        }
+
+        ::slotted(img), ::slotted(div) {
+          width: 100%;
+          height: 100%;
+        }
       </style>
-      <div></div>
+      <slot></slot>
     `;
   }
 }
