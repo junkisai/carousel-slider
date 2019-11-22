cef57b36c35f4078242fcb363084f60415ec652e
junkisai <jonki.beam@gmail.com>
2019-11-22 17:13:50 +0900

    update; style

--- src/components/carousel-slider-item.js
+++ src/components/carousel-slider-item.js
@@ -13,11 +13,10 @@ class CarouselSliderItem extends LitElement {
     return html`
       <style>
         :host {
-          border: solid 1px #0fdf75;
           flex-shrink: 0;
         }
 
-        ::slotted(img), ::slotted(div) {
+        ::slotted(:host) {
           width: 100%;
           height: 100%;
         }
